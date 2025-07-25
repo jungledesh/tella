8a64d8c32c8da9c25792b6c4bbc908a0865662ac
Perry <perry@Perrys-MacBook-Pro.local>
2025-07-25 15:42:29 -0700

    basic server

--- src/db.ts
+++ src/db.ts
@@ -2,16 +2,17 @@
 import pkg from 'sqlite3';
 const { Database } = pkg;
 
-// Create or open DB file (local mocking).
+// Create or open DB file (local mocking)
 const db = new Database('./tella.db');
 
 // Init users table if not exists (hashed state for users).
+// Init users table if not exists.
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS users (
       phone_hash TEXT PRIMARY KEY,
-      pin_hash TEXT,
-      wallet_init BOOLEAN DEFAULT 0
+      wallet_init BOOLEAN DEFAULT 0, 
+      pending_actions TEXT
     )
   `);
 });
